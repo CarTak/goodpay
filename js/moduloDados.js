571947925e06de9ce749424d6c56542cd15aa8b1
CarTak <cartakkun@gmail.com>
2022-09-30 18:44:28 -0300

    aula 30-09-22

---
+++ js/moduloDados.js
@@ -0,0 +1,38 @@
+/*
+* Dentro deste array existem usuarios com esta estrutura:
+* - id
+* - nomeUsuario
+* - senha
+* - email
+*/
+const usuarios = [
+    {
+        id: 1,
+        nomeUsuario: "jose-almir",
+        senha: "12345",
+        email: "jose.almir@soulacademy.org",
+    },
+    {
+        id: 2,
+        nomeUsuario: "renato3x",
+        senha: "12345",
+        email: "renato.pereira@gmail",
+    },
+    {
+        id: 3,
+        nomeUsuario: "bruno-dias",
+        senha: "12345",
+        email: "bruno.dias@gmail.com",
+    },
+    {
+        id: 4,
+        nomeUsuario: "pedro-viana",
+        senha: "12345",
+        email: "pedro.viana@gmail.com",
+    },
+]
+
+export default usuarios;
+/*
+* Disponibiliza o array para ser utilizado fora do arquivo moduloDados.js
+*/
