e8158a77345124ff48b8e83000424649c3920863
CarTak <cartakkun@gmail.com>
2022-10-03 17:17:35 -0300

    aula 03-10-22

--- js/index.js
+++ js/index.js
@@ -2,6 +2,29 @@
 * Trouxe o array do arquivo moduloDados.js
 * para o index.js
 */
-import usuarios from "./moduloDados.js";
+import usuarios from "./moduloDados.js"
 
-console.log(usuarios)
+const btnEntrar = document.getElementById("btnEntrar")
+//ele utiliza o modal pronto do bootstrap
+const alertaLogin = new bootstrap.Modal("#alertaLogin")
+
+btnEntrar.addEventListener("click", () => {
+    const inputLogin = document.getElementById("login").value
+    const inputSenha = document.getElementById("senha").value
+
+    let usuarioValido
+   
+    for(let usuario of usuarios){
+        if(usuario.nomeUsuario == inputLogin) {
+            usuarioValido = usuario
+            break
+        }
+    }
+
+    if(inputSenha == usuarioValido.senha) {
+        // redireciona o usuário para essa página html
+        window.location.pathname = '/app.html'
+    } else {
+        alertaLogin.show()
+    }
+})
